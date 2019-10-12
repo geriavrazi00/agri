2fccd8e90aea1d12bdcb2d2be62ded58f8522ebc
Geri Avrazi <geri.avrazi@gmail.com>
2019-10-12 19:40:32 +0200

    Added the technology migration and model

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,10 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    if(Auth::check())
+		return redirect('/home');
+    else 
+    	return view('auth/login');
 });
 
 Auth::routes();
