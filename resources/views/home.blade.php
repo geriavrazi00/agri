4e1b535ff65f5a3d773f663779c58d93cf4a3b47
irsasaraci <irsasaraci@users.noreply.github.com>
2019-11-10 20:27:46 +0100

    changed table head and format, and table description format

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -29,71 +29,75 @@
                 </div>
                 <!-- Table Heading End -->
                 <!--Short table information-->
-                <div>
+                <div id="tabledesc">
                     <p>
                         Aktivitetet bujqesore kane intensitete zhvillimi te ndryshme bazuar ne pozicionin gjeografik te fermes, sherbimeve
                         agroteknike qe perdor, menyren e kultivimit (intensive apo tradicionale), faktoreve klimaterik etj.
                         Kjo platforme te mundeson te perzgjedhesh nivelin operues te fermes
                     </p>
+
                 </div>
 
-                @for($i = 0; $i < sizeof($categories); $i++)
-                    <div class="button">
-                        <div class="rtable-cell item-cell-type">
-                            <a href="#" onclick="selectCategories('{{$categories[$i]->id}}');">
-                                <img src="img/product-images/blegtori.png" style="width:100px;height:100px;"/>
-                            </a>
-                            <input class="form-control in-odd-row item-name" hidden name="item-name-1" type="text" value="Product or Service Name 1" />
-                        </div>
-                        <p>{{$categories[$i]->name}}</p>
+
+
+                @for($i = 0; $i < sizeof($categories); $i++) <div class="button">
+                    <div class="rtable-cell item-cell-type">
+                        <a href="#" onclick="selectCategories('{{$categories[$i]->id}}');">
+                            <img src="img/product-images/sera.png" style="width:100px;height:100px;" />
+                        </a>
+                        <input class="form-control in-odd-row item-name" hidden name="item-name-1" type="text" value="Product or Service Name 1" />
                     </div>
-                @endfor
-
-                <div id="category-{{$categories[0]->id}}" style="display: none;">
-                    @include('inputs')
-                </div>
+                    <p>{{$categories[$i]->name}}</p>
             </div>
+            @endfor
+
+            <div id="category-{{$categories[0]->id}}" style="display: none;">
+                @include('inputs')
+            </div>
+
         </div>
-    </div>
+
+
+        <div class="rtable rtable--collapse">
+
+            <!-- Table Heading Start -->
+            <div class="rtable-row rtable-row--head">
+                <div class="rtable-cell item-cell column-heading" style="text-align:center">{{trans('messages.enter_data')}}</div>
+            </div>
+            <!-- Table Heading End -->
+            <div class="card-body">
+                <form method="POST" action="/result">
+                    @csrf
+                    <label for="applicant-name">{{trans('messages.applicant_name')}}</label>
+                    <input class="form-control" type="text" name="applicant-name" />
+
+                    <label for="farm-type">{{trans('messages.farm_type')}}</label>
+                    <select class="form-control" id="farm-type" name="farm-type" onchange="chooseCategory();">
+                        <option value="null">{{trans('messages.none')}}</option>
+                        @for($i = 0; $i < sizeof($categories); $i++) <option value="{{$categories[$i]->id}}">{{$categories[$i]->name}}</option>
+                            @endfor
+                    </select>
+
+                    <br />
+
+                    <fieldset>
+                        <legend>Zgjidh ndonjë gjë</legend>
+                        @for($i = 0; $i < sizeof($categories); $i++) <div>
+                            <input type="checkbox" id="{{$categories[$i]->id}}" name="{{$categories[$i]->id}}" value="{{$categories[$i]->id}}" />
+                            <label for="{{$categories[$i]->id}}">{{$categories[$i]->name}}</label>
+            </div>
+            @endfor
+            </fieldset>
+
+        </div>
 </section>
 
-<div style="padding: 20px; display: none;">
-    <div class="row justify-content-center">
-        <div class="col-md-12">
-            <div class="card">
-                <div class="card-header">{{trans('messages.enter_data')}}</div>
 
-                <div class="card-body">
-                    <form method="POST" action="/result">
-                        @csrf
-                        <label for="applicant-name">{{trans('messages.applicant_name')}}</label>
-                        <input class="form-control" type="text" name="applicant-name"/>
-
-                        <label for="farm-type" >{{trans('messages.farm_type')}}</label>
-                        <select class="form-control" id="farm-type" name="farm-type" onchange="chooseCategory();" >
-                            <option value="null">{{trans('messages.none')}}</option>
-                            @for($i = 0; $i < sizeof($categories); $i++)
-                                <option value="{{$categories[$i]->id}}">{{$categories[$i]->name}}</option>
-                            @endfor
-                        </select>
-
-                        <br/>
-
-                        <fieldset>
-                            <legend>Zgjidh ndonjë gjë</legend>
-                            @for($i = 0; $i < sizeof($categories); $i++)
-                                <div>
-                                    <input type="checkbox" id="{{$categories[$i]->id}}" name="{{$categories[$i]->id}}" value="{{$categories[$i]->id}}"/>
-                                    <label for="{{$categories[$i]->id}}">{{$categories[$i]->name}}</label>
-                                </div>
-                            @endfor
-                        </fieldset>
-
-                        @include('inputs')
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
+@include('inputs')
+</form>
+</div>
+</div>
+</div>
+</div>
 </div>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,84 +1,74 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <!-- CSRF Token -->
-        <meta name="csrf-token" content="{{ csrf_token() }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <!-- Scripts -->
-        <script src="{{ asset('js/app.js') }}" defer></script>
-        <script src="{{ asset('js/agri.js') }}" defer></script>
-        <script src="{{ asset('js/scripts.min.js') }}" defer></script>
-        <script src="{{ asset('js/scripts.order-2.min.js') }}" defer></script>
-        <script src="{{ asset('js/min/main.min.js') }}" defer></script>
-        <script src="{{ asset('js/min/order-2.min.js') }}" defer></script>
+    <title>{{ config('app.name', 'Laravel') }}</title>
 
-        <!-- Fonts -->
-        <link rel="dns-prefetch" href="//fonts.gstatic.com">
-        <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="{{ asset('js/agri.js') }}" defer></script>
+    <script src="{{ asset('js/scripts.min.js') }}" defer></script>
+    <script src="{{ asset('js/scripts.order-2.min.js') }}" defer></script>
+    <script src="{{ asset('js/min/main.min.js') }}" defer></script>
+    <script src="{{ asset('js/min/order-2.min.js') }}" defer></script>
 
-        <!-- Styles -->
-        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-        <link href="{{ asset('css/main.blue.css') }}" rel="stylesheet">
-        <link href="{{ asset('css/order-2.blue.css') }}" rel="stylesheet">
-        <link href="{{ asset('css/styles.blue.min.css') }}" rel="stylesheet">
-        
-    </head>
-    <body>
-        <div id="app" style="padding-top: 0px;">
-            <!-- Preloader -->
-            <div class="preloader"></div>
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
-            <!-- Navbar -->
-            <header class="header">
-                <nav class="navbar navbar-expand-lg fixed-top">
-                    <div class="container">
-                        <a class="navbar-brand" href="./"><img alt="" class="img-fluid" src="img/logoafa.png" style="width:50px;height:60px;"></a>
-                        <button aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation" class="navbar-toggler navbar-toggler-right" data-target="#navbarSupportedContent" data-toggle="collapse" type="button">Menu<i class="fa fa-bars ml-2"></i>
-                        </button>
-                        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                            <ul class="navbar-nav ml-auto">
-                                <!-- Link -->
-                                <li class="nav-item">
-                                    <a class="nav-link" href="index.html">Home</a>
-                                </li>
-                                <!-- Drop Down -->
-                                <li class="nav-item dropdown">
-                                    <a aria-expanded="false" aria-haspopup="true" class="nav-link active dropdown-toggle" data-toggle="dropdown" href="" id="pages">Orer Wizard</a>
-                                    <div class="dropdown-menu">
-                                        <a class="dropdown-item" href="order-1.html">Demo 1</a>
-                                        <a class="dropdown-item" href="order-2.html">Demo 2</a>
-                                        <a class="dropdown-item" href="order-3.html">Demo 3</a>
-                                    </div>
-                                </li>
-                                <!-- Link -->
-                                <li class="nav-item">
-                                    <a class="nav-link" href="faq.html">FAQ</a>
-                                </li>
-                            </ul>
-                            <a class="btn btn-primary navbar-btn ml-0 ml-lg-3" href="#">BUY</a>
-                        </div>
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/main.blue.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/order-2.blue.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/styles.blue.min.css') }}" rel="stylesheet">
+
+</head>
+
+<body>
+    <div id="app" style="padding-top: 0px;">
+        <!-- Preloader -->
+        <div class="preloader"></div>
+
+        <!-- Navbar -->
+        <header class="header">
+            <nav class="navbar navbar-expand-lg fixed-top">
+                <div class="container">
+                    <a class="navbar-brand" href="./"><img alt="" class="img-fluid" src="img/logoafa.png" style="width:50px;height:60px;"></a>
+                    <button aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation" class="navbar-toggler navbar-toggler-right" data-target="#navbarSupportedContent" data-toggle="collapse" type="button">Menu<i class="fa fa-bars ml-2"></i>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="navbar-nav ml-auto">
+                            <!-- Link -->
+                            <li class="nav-item">
+                                <a class="nav-link" href="index.html">Home</a>
+                            </li>
+                        </ul>
+                        <a class="btn btn-primary navbar-btn ml-0 ml-lg-3" href="#">Afa</a>
                     </div>
-                </nav>
-            </header>
+                </div>
+            </nav>
+        </header>
 
-            <main class="py-4">
-                @yield('content')
-            </main>
-        </div>
+        <main class="py-4">
+            @yield('content')
+        </main>
+    </div>
 
-        <script src="{{ asset('js/jquery/jquery.min.js') }}"></script>
-        <script src="{{ asset('js/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
-        <script src="{{ asset('js/lightbox2/js/lightbox.min.js') }}"></script>
-        <script src="{{ asset('js/easing/easing.min.js') }}"></script>
-        <script src="{{ asset('js/parsley/parsley.min.js') }}" defer></script>
-        <script src="{{ asset('js/scrollreveal/scrollreveal.min.js') }}" defer></script>
+    <script src="{{ asset('js/jquery/jquery.min.js') }}"></script>
+    <script src="{{ asset('js/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('js/lightbox2/js/lightbox.min.js') }}"></script>
+    <script src="{{ asset('js/easing/easing.min.js') }}"></script>
+    <script src="{{ asset('js/parsley/parsley.min.js') }}" defer></script>
+    <script src="{{ asset('js/scrollreveal/scrollreveal.min.js') }}" defer></script>
 
-        <script src="{{ asset('js/main.js') }}" defer></script>
-        <script src="{{ asset('js/order-2.js') }}" defer></script>
-    </body>
+    <script src="{{ asset('js/main.js') }}" defer></script>
+    <script src="{{ asset('js/order-2.js') }}" defer></script>
+</body>
+
 </html>
